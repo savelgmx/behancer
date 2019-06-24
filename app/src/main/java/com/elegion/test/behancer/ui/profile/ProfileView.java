a37724a9be9440944705800c54c2972e6366a193
savelgmx <savel@gmx.com>
2019-06-24 10:45:39 +0700

    Commit#3 Созданы ProfilePresenter и ProfileView

---
+++ app/src/main/java/com/elegion/test/behancer/ui/profile/ProfileView.java
@@ -0,0 +1,10 @@
+package com.elegion.test.behancer.ui.profile;
+
+import com.elegion.test.behancer.common.BaseView;
+
+/**
+ * Created by andrew on 23.06.2019.
+ */
+
+public interface ProfileView extends BaseView {
+}
