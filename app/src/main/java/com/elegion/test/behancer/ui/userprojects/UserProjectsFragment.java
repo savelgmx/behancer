ccf492c645a93be89c1e90ae10a08a178c2c1b12
savelgmx <savel@gmx.com>
2019-08-15 16:48:20 +0700

    Commit#16 moxy
    //Storage initialize

--- app/src/main/java/com/elegion/test/behancer/ui/userprojects/UserProjectsFragment.java
+++ app/src/main/java/com/elegion/test/behancer/ui/userprojects/UserProjectsFragment.java
@@ -3,17 +3,21 @@ package com.elegion.test.behancer.ui.userprojects;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.arellomobile.mvp.presenter.InjectPresenter;
+import com.arellomobile.mvp.presenter.ProvidePresenter;
 import com.elegion.test.behancer.R;
 import com.elegion.test.behancer.common.BasePresenter;
 import com.elegion.test.behancer.common.PresenterFragment;
+import com.elegion.test.behancer.common.RefreshOwner;
 import com.elegion.test.behancer.common.Refreshable;
 import com.elegion.test.behancer.data.Storage;
 import com.elegion.test.behancer.data.model.userprojects.UserProjects;
+import com.elegion.test.behancer.ui.projects.ProjectsAdapter;
 
 import java.util.List;
 
@@ -29,11 +33,29 @@ public class UserProjectsFragment extends PresenterFragment
         implements Refreshable,UserProjectsView{
 
     public static final String USER_PROJECTS_KEY = "USER_PROJECTS_KEY";
-/*
-    @InjectPresenter*/
-    private UserProjectsPresenter mPresenter;
+
+    private RecyclerView mRecyclerView;
+    private RefreshOwner mRefreshOwner;
+    private View mErrorView;
+    private ProjectsAdapter mProjectsAdapter;
+
+
     private Storage mStorage;
 
+    /* private*/
+
+    @InjectPresenter
+    UserProjectsPresenter mPresenter;
+
+    @ProvidePresenter
+    UserProjectsPresenter providePresenter(){
+        return new UserProjectsPresenter(mStorage);
+    }
+
+    @Override
+    protected UserProjectsPresenter getPresenter(){
+        return mPresenter;
+    }
 
 public static UserProjectsFragment newInstance(Bundle args){
     UserProjectsFragment fragment = new UserProjectsFragment();
@@ -79,23 +101,24 @@ public static UserProjectsFragment newInstance(Bundle args){
 
     }
 
-    @Override
-    protected BasePresenter getPresenter() {
-        return null;
-    }
+
 
     @Override
     public void showRefresh() {
+
+        mRefreshOwner.setRefreshState(true);
 
     }
 
     @Override
     public void hideRefresh() {
-
+        mRefreshOwner.setRefreshState(false);
     }
 
     @Override
     public void showError() {
+        mErrorView.setVisibility(View.VISIBLE);
+        mRecyclerView.setVisibility(View.GONE);
 
     }
 
@@ -103,6 +126,9 @@ public static UserProjectsFragment newInstance(Bundle args){
 
     @Override
     public void showUserProjects(List<UserProjects> userprojects) {
+        mErrorView.setVisibility(View.GONE);
+        mRecyclerView.setVisibility(View.VISIBLE);
+      //  mProjectsAdapter.addData(userprojects,true);
 
     }
 }
--- app/src/main/java/com/elegion/test/behancer/ui/userprojects/UserProjectsPresenter.java
+++ app/src/main/java/com/elegion/test/behancer/ui/userprojects/UserProjectsPresenter.java
@@ -1,5 +1,6 @@
 package com.elegion.test.behancer.ui.userprojects;
 
+import com.arellomobile.mvp.InjectViewState;
 import com.elegion.test.behancer.BuildConfig;
 import com.elegion.test.behancer.common.BasePresenter;
 import com.elegion.test.behancer.data.Storage;
@@ -12,7 +13,7 @@ import io.reactivex.schedulers.Schedulers;
 /**
  * Created by andrew on 13.07.2019.
  */
-
+@InjectViewState
 public class UserProjectsPresenter extends BasePresenter<UserProjectsView> {
 
     private final Storage mStorage;
